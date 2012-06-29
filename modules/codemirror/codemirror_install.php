be6a6b3f984831be06e53f6cdd3c1fa6b7f41659
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-06-29 00:09:41 +0000

    - Removed Codemirror module
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@12 68377534-e894-4f14-aad3-e92cf46e58fa

--- modules/codemirror/codemirror_install.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$module_info['name'] = 'CodeMirror Template Editor';
-	$module_info['desc'] = 'CodeMirror is a JavaScript library that parses and styles code markup for the admin template editor.';
-	$module_info['version'] = 0.1;
-?>
