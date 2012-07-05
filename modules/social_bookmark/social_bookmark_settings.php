4a3dab33d58122355b3c4589959dac767333d3ba
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-07-05 18:17:39 +0000

    - Fixed a language file call in simple messaging module, even though I don't think it was generating an error.
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@37 68377534-e894-4f14-aad3-e92cf46e58fa

--- modules/social_bookmark/social_bookmark_settings.php
+++ modules/social_bookmark/social_bookmark_settings.php
@@ -20,15 +20,12 @@ define('social_bookmark_pligg_lang_conf', lang_loc . "/languages/lang_" . pligg_
 
 // the path to the modules templates. the probably shouldn't be changed unless you rename the social_bookmark folder(s)
 define('social_bookmark_tpl_path', '../modules/social_bookmark/templates/');
-// the path for smarty / template lite plugins
-define('social_bookmark_plugins_path', 'modules/social_bookmark/plugins');
-
-// don't touch anything past this line.
 
 if(is_object($main_smarty)){
 	$main_smarty->assign('social_bookmark_path', social_bookmark_path);
 	$main_smarty->assign('social_bookmark_lang_conf', social_bookmark_lang_conf);
 	$main_smarty->assign('social_bookmark_tpl_path', social_bookmark_tpl_path);
+	$main_smarty->assign('social_bookmark_pligg_lang_conf', social_bookmark_pligg_lang_conf);
 }
 
 ?>
