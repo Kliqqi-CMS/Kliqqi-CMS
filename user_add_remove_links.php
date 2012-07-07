f5ab04ae0639bf9ece09feb515de4b93c04e9d8c
ankanmitra <ankanmitra@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-07-07 06:32:34 +0000

    Save and Remove links in the story toolbar should change
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@54 68377534-e894-4f14-aad3-e92cf46e58fa

--- user_add_remove_links.php
+++ user_add_remove_links.php
@@ -24,13 +24,14 @@ global $db;
 
 if($current_user->user_id != 0)
 {	
-	$action = isset($_GET['action']) ? sanitize($_GET['action'], 3) : '';
+	$action = isset($_POST['action']) ? sanitize($_POST['action'], 3) : '';
 	if($action == 'add') {	
 		
-		if(!($linkid = check_integer('link')))
+		/*if(!($linkid = check_integer('link_id')))
 		{	
 			die("Invalid Link ID");
-		}
+		}*/
+		$linkid=$_POST['link_id'];
 		$link = new Link;
 		$link->id=$linkid;
 		$link->read();
@@ -40,14 +41,16 @@ if($current_user->user_id != 0)
 		{
 			$sql="INSERT INTO " . table_saved_links . " (saved_user_id, saved_link_id) VALUES ($current_user->user_id, $linkid)";
 			$db->query($sql);
+			echo "1";
 		}
 		
 	} elseif ($action == 'remove') {
 		
-		if(!($linkid = check_integer('link')))
+		/*if(!($linkid = check_integer('link')))
 		{	
 			die("Invalid Link ID");
-		}
+		}*/
+		$linkid=$_POST['link_id'];
 		$link = new Link;
 		$link->id=$linkid;
 		$link->read();
@@ -57,6 +60,7 @@ if($current_user->user_id != 0)
 		{
 			$sql="DELETE FROM " . table_saved_links . " WHERE saved_user_id=$current_user->user_id AND saved_link_id=$linkid";
 			$db->query($sql);
+			echo "2";
 		}
 		
 	}
