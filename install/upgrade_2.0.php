c79d3d5bf942fbf6500e58bfdeb83f0bb01d969e
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-07-21 03:16:27 +0000

    - I forgot to include this very important file with the last release.
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@110 68377534-e894-4f14-aad3-e92cf46e58fa

---
+++ install/upgrade_2.0.php
@@ -0,0 +1,28 @@
+<?php
+// This file is for performing an upgrade from Pligg 1.0 to 2.0.
+
+// Get your Pligg Version
+$sql = "SELECT data FROM " . table_misc_data . " WHERE name = 'pligg_version'";
+$pligg_version = $db->get_var($sql);
+
+$old_version = str_replace('.', '' , $pligg_version);
+$new_version = '200';
+
+// Check if you need to run the one time upgrade to Pligg 2.0
+if ($old_version < $new_version) {
+	//echo $lang['UpgradingTables'] . '<br />';
+	echo '<li>Performing one-time Pligg 2.0 Upgrade</li><ul>';
+
+	$sql = "UPDATE ".table_users." SET user_level='moderator' WHERE user_level='admin';";
+	$db->query($sql);
+	echo '<li>Changed Admin to Moderator</li>';
+
+	$sql = "UPDATE ".table_users." SET user_level='admin' WHERE user_level='admin';";
+	$db->query($sql);
+	echo '<li>Changed God to Admin</li>';
+	
+	// Finished 2.0 upgrade
+	echo'</ul>';
+}
+
+?>
