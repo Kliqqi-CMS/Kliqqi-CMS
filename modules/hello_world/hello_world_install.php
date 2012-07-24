f772e071c32f146a6b4af77e470bb6208da5421d
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-07-24 09:39:00 +0000

    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@137 68377534-e894-4f14-aad3-e92cf46e58fa

--- modules/hello_world/hello_world_install.php
+++ modules/hello_world/hello_world_install.php
@@ -1,10 +1,7 @@
 ﻿<?php
-
 	$module_info['name'] = 'Hello World';
-	$module_info['desc'] = 'Welcomes the site Admin to Pligg. It should automatically disable itself after closing the automatic popup on the first admin homepage visit.';
-	$module_info['version'] = 0.3;
-	$module_info['update_url'] = 'http://forums.pligg.com/versioncheck.php?product=helloworld';
-	$module_info['homepage_url'] = 'http://forums.pligg.com/pligg-modules/15515-hello-world.html';
-	$module_info['requires'][] = array('admin_help_english', 0.1, 'Admin Help English', '');
-
+	$module_info['desc'] = 'This module will insert some helpful hints for those first starting out with Pligg. It uses browser cookies so that you should only see each hint once.';
+	$module_info['version'] = 2.0;
+	$module_info['update_url'] = '';
+	$module_info['homepage_url'] = '';
 ?>
