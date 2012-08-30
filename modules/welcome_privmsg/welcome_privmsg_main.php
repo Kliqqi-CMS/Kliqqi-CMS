347d989eceb531e341d5aeaf89ad51769adf1b0d
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-08-30 16:18:25 +0000

    - Trying to fix path to kmessaging
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@404 68377534-e894-4f14-aad3-e92cf46e58fa

--- modules/welcome_privmsg/welcome_privmsg_main.php
+++ modules/welcome_privmsg/welcome_privmsg_main.php
@@ -1,10 +1,11 @@
 <?php
+
 function welcome_privmsg_send(&$registration_details)
 {
 	global $username, $main_smarty, $current_user;
 
 	include_once(mnminclude.'user.php');
-	include_once(mnminclude.'../modules/simple_messaging/kmessaging/class.KMessaging.php');
+	include_once('../simple_messaging/kmessaging/class.KMessaging.php');
 
 	$siteName = $main_smarty->get_config_vars('PLIGG_Visual_Name'); 
 	
