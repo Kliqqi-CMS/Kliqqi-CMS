aa9d4d3d1926f9b2c4e9de9d54ac1cb7872e4ba7
ankanmitra <ankanmitra@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-09-14 13:43:38 +0000

    Change to version number for testing updates notification
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@455 68377534-e894-4f14-aad3-e92cf46e58fa

--- modules/fb/fb_install.php
+++ modules/fb/fb_install.php
@@ -1,7 +1,7 @@
 <?php
 	$module_info['name'] = 'Facebook ';
 	$module_info['desc'] = 'Lets members use Facebook Connect to login or register an account faster.';
-	$module_info['version'] = 4.2;
+	$module_info['version'] = 3.0;
 	$module_info['settings_url'] = '../module.php?module=fb';
 	$module_info['homepage_url'] = 'http://forums.pligg.com/modules-sale/16197-facebook-connect-module.html';
 	$module_info['update_url'] = 'http://forums.pligg.com/versioncheck.php?product=facebook';
