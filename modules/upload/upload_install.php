695ac1e831158d0efcc552e7d431747b404e228f
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-09-19 01:08:39 +0000

    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@477 68377534-e894-4f14-aad3-e92cf46e58fa

--- modules/upload/upload_install.php
+++ modules/upload/upload_install.php
@@ -1,6 +1,6 @@
 <?php
 	$module_info['name'] = 'Upload';
-	$module_info['desc'] = 'Allows you to attach images and files to an article';
+	$module_info['desc'] = 'Attach images and files to an article';
 	$module_info['version'] = 2.00;
 	$module_info['update_url'] = 'http://forums.pligg.com/versioncheck.php?product=upload';
 	$module_info['homepage_url'] = 'http://forums.pligg.com/free-modules/13594-upload-module-file-image-attachment.html';
