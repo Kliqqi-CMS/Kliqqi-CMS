5ce435c03850d7e841cf268063065427f4025fa0
ankanmitra <ankanmitra@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-10-09 12:20:35 +0000

    bug fixing
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@574 68377534-e894-4f14-aad3-e92cf46e58fa

--- load_data_for_groups.php
+++ load_data_for_groups.php
@@ -3,32 +3,24 @@
 	$main_smarty = new Smarty;
 	
 	include('config.php');
-include(mnminclude.'html1.php');
-include(mnminclude.'link.php');
-include(mnminclude.'group.php');
-include(mnminclude.'smartyvariables.php');
+	include(mnminclude.'html1.php');
+	include(mnminclude.'link.php');
+	include(mnminclude.'group.php');
+	include(mnminclude.'smartyvariables.php');
 	
 	$group_display = "";
 	
-	$offset = isset($_REQUEST['start_up'])? $_REQUEST['start_up'] : "";
+	$start_up = isset($_REQUEST['start_up'])? $_REQUEST['start_up'] : "";
 	$page_size = isset($_REQUEST['pagesize']) ? $_REQUEST['pagesize'] : "";
-	
-	$group = $db->get_results("SELECT * FROM " . table_groups . " WHERE group_status='Enable' ORDER BY group_status DESC LIMIT $offset, $page_size");
-	
-	
-	//echo "<pre>";
-	//echo "SELECT * FROM " . table_groups . " WHERE group_status='Enable' ORDER BY group_status DESC LIMIT $offset, $page_size";
-	//print_r($group);
+
+	$group = $db->get_results("SELECT * FROM " . table_groups . " WHERE group_status='Enable' ORDER BY group_status, group_date  DESC LIMIT $start_up, $page_size");
 	
 	if ($group)
 	{
-		
 		foreach($group as $groupid)
 		{
-			//echo "Hello";
 			$group_display .= group_print_summary($groupid->group_id);
 		}
 		echo $group_display;
 	}
-	
 ?>
