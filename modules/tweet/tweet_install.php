f5cd20e77ba6a397df01ff53be22c704d27023b2
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-10-23 07:09:18 +0000

    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@636 68377534-e894-4f14-aad3-e92cf46e58fa

--- modules/tweet/tweet_install.php
+++ modules/tweet/tweet_install.php
@@ -2,10 +2,9 @@
 	$module_info['name'] = 'Tweet Module';
 	$module_info['desc'] = 'This module will post stories to a Twitter account whenever a story is either published or submitted.';
 	$module_info['version'] = 1.1;
-//	$module_info['homepage_url'] = 'http://www.pligg.com/pro/catalog/modules/karma-98.html';
+	$module_info['homepage_url'] = 'http://forums.pligg.com/modules-sale/17885-tweet-automatic-twitter-submissions-5.html#post106993';
 	$module_info['settings_url'] = '../module.php?module=tweet';
-	// this is where you set the modules "name" and "version" that is required
-	// if more that one module is required then just make a copy of that line
+	$module_info['update_url'] = 'http://forums.pligg.com/versioncheck.php?product=tweet';
 
 	$module_info['db_add_field'][]=array(table_prefix . 'links', 'tweet_id', 'VARCHAR',  32, "", 0, '0');
 ?>
