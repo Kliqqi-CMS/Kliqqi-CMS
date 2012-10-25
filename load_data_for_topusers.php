90a1f000284cd687f64d1d0d27d6f09b330d9f2f
ankanmitra <ankanmitra@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-10-25 12:32:55 +0000

    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@657 68377534-e894-4f14-aad3-e92cf46e58fa

--- load_data_for_topusers.php
+++ load_data_for_topusers.php
@@ -15,9 +15,6 @@
 	$start_up = isset($_REQUEST['start_up']) ? $_REQUEST['start_up'] : "";
 	$page_size = isset($_REQUEST['pagesize']) ? $_REQUEST['pagesize'] : "";
 	
-	echo "SELECT user_id FROM pligg_users WHERE user_karma > 0 AND user_enabled = 1 AND user_level <> 'Spammer' AND (user_login != 'anonymous' OR user_lastip) ORDER BY user_karma DESC LIMIT $start_up, $page_size";
-	die;
-	
 $users = $db->get_results("SELECT user_karma, COUNT(*) FROM " . table_users . " WHERE user_karma > 0 $whether_to_show_user GROUP BY user_karma ORDER BY user_karma DESC", ARRAY_N);
 	
 	$ranklist = array();
@@ -30,7 +27,7 @@ $users = $db->get_results("SELECT user_karma, COUNT(*) FROM " . table_users . " 
 		}
 		
 		
-	$users = $db->get_results("SELECT user_id FROM pligg_users WHERE user_karma > 0 AND user_enabled = 1 AND user_level <> 'Spammer' AND (user_login != 'anonymous' OR user_lastip) ORDER BY user_karma DESC LIMIT $start_up, $page_size");
+	$users = $db->get_results("SELECT user_id FROM ".table_users." WHERE user_karma > 0 AND user_enabled = 1 AND user_level <> 'Spammer' AND (user_login != 'anonymous' OR user_lastip) ORDER BY user_karma DESC LIMIT $start_up, $page_size");
 	
 	if ($users) {
 	
