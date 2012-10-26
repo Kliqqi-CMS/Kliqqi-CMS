5306e18245138957e813a573a31ddb2bb6556ed2
ankanmitra <ankanmitra@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-10-26 13:07:18 +0000

    changes for sorting
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@668 68377534-e894-4f14-aad3-e92cf46e58fa

--- libs/smartyvariables.php
+++ libs/smartyvariables.php
@@ -213,6 +213,10 @@ if ($current_user->user_id > 0 && $current_user->authenticated)
 	$main_smarty->assign('user_url_news_published', getmyurl('user2', $login, 'published'));
 	$main_smarty->assign('user_url_news_unpublished', getmyurl('user2', $login, 'shaken'));
 	$main_smarty->assign('user_url_news_voted', getmyurl('user2', $login, 'voted'));
+	
+	$main_smarty->assign('user_url_news_upvoted', getmyurl('user2', $login, 'upvoted'));
+	$main_smarty->assign('user_url_news_dwnvoted', getmyurl('user2', $login, 'dwnvoted'));
+	
 	$main_smarty->assign('user_url_commented', getmyurl('user2', $login, 'commented'));
 	$main_smarty->assign('user_url_saved', getmyurl('user2', $login, 'saved'));
 	$main_smarty->assign('user_url_setting', getmyurl('profile'));
