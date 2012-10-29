8db3367ce94dee5aee449f014dc6d68f1dce4e1e
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-10-29 20:19:08 +0000

    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@700 68377534-e894-4f14-aad3-e92cf46e58fa

--- libs/check_behind_proxy.php
+++ libs/check_behind_proxy.php
@@ -3,25 +3,25 @@
 if(!defined('mnminclude')){header('Location: ../404error.php');die();}
 
 function isIPIn($ip,$net,$mask) {
-        $lnet=ip2long($net);
-        $lip=ip2long($ip);
-        $binnet=str_pad( decbin($lnet),32,"0",STR_PAD_LEFT );
-        $firstpart=substr($binnet,0,$mask);
-        $binip=str_pad( decbin($lip),32,"0",STR_PAD_LEFT );
-        $firstip=substr($binip,0,$mask);
-        return(strcmp($firstpart,$firstip)==0);
+	$lnet=ip2long($net);
+	$lip=ip2long($ip);
+	$binnet=str_pad( decbin($lnet),32,"0",STR_PAD_LEFT );
+	$firstpart=substr($binnet,0,$mask);
+	$binip=str_pad( decbin($lip),32,"0",STR_PAD_LEFT );
+	$firstip=substr($binip,0,$mask);
+	return(strcmp($firstpart,$firstip)==0);
 }
 
 
 function isPrivateIP($ip) {
-        $privates = array ("127.0.0.0/24", "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16");
-        foreach ( $privates as $k ) {
-                list($net,$mask)=split("/",$k);
-                if (isIPIn($ip,$net,$mask)) {
-                        return true;
-                }
-        }
-        return false;
+	$privates = array ("127.0.0.0/24", "10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16");
+	foreach ( $privates as $k ) {
+			list($net,$mask)=split("/",$k);
+			if (isIPIn($ip,$net,$mask)) {
+					return true;
+			}
+	}
+	return false;
 }
 
 function check_ip_behind_proxy() {
