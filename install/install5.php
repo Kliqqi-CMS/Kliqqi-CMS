e868ff320fb6763484ae304bd981a7752f886fef
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2012-10-29 19:44:30 +0000

    - Admin IP is added to approvedips.log during installation
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@698 68377534-e894-4f14-aad3-e92cf46e58fa

--- install/install5.php
+++ install/install5.php
@@ -49,6 +49,35 @@ if (!$errors) {
 		}
 	}
 	
+	// Add user IP address to approved IP list, so they are never blocked for bad logins
+	function get_ip_address() {
+		foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key) {
+			if (array_key_exists($key, $_SERVER) === true) {
+				foreach (explode(',', $_SERVER[$key]) as $ip) {
+					if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
+						return $ip;
+					}
+				}
+			}
+		}
+	}
+	$approvedips = '../logs/approvedips.log';
+	if (file_exists($approvedips)) {
+		$user_ip = get_ip_address();
+		if ($user_ip){
+			$filedata = "$user_ip \n";
+			// print $filedata;
+			// echo 'IP: '.get_ip_address();
+			
+			// Write to the approvedips log file
+			$ip_file = fopen($approvedips, "w");
+			fwrite($ip_file, $filedata);
+			fclose($ip_file);
+			
+		}
+	}
+
+	
 	// Output success message
 	$output='<p><strong>' . $lang['InstallSuccess'] . '</strong></p>
 	<br /><legend>' . $lang['WhatToDo'] . '</legend>
