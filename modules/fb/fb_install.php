db863b74610d44615906840ebf269de80b9a8f93
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2013-02-12 18:53:22 +0000

    - Facebook module updates
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@773 68377534-e894-4f14-aad3-e92cf46e58fa

--- modules/fb/fb_install.php
+++ modules/fb/fb_install.php
@@ -1,7 +1,7 @@
 <?php
 	$module_info['name'] = 'Facebook ';
 	$module_info['desc'] = 'New members can register by logging in to their Facebook account, while existing members can connect their account to Facebook for a faster login process.';
-	$module_info['version'] = 4.2;
+	$module_info['version'] = 4.3;
 	$module_info['settings_url'] = '../module.php?module=fb';
 	$module_info['homepage_url'] = 'http://forums.pligg.com/modules-sale/16197-facebook-connect-module.html';
 	$module_info['update_url'] = 'http://forums.pligg.com/versioncheck.php?product=facebook';
