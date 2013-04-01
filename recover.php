eb901abe2e29d03633e0598203beba0ddbf90366
yankidank <yankidank@68377534-e894-4f14-aad3-e92cf46e58fa>
2013-04-01 04:53:38 +0000

    - Trying to improve the formatting of recover.php
    git-svn-id: https://pligg.svn.beanstalkapp.com/svnroot/trunk@817 68377534-e894-4f14-aad3-e92cf46e58fa

--- recover.php
+++ recover.php
@@ -53,59 +53,57 @@ if($user){
 			
 			if($error==false){
 			
-			$saltedlogin = generateHash($username);
-					
-								
-						$to = $user->user_email;
-						$subject = $main_smarty->get_config_vars("PLIGG_Visual_Name").' '.$main_smarty->get_config_vars("PLIGG_PassEmail_Subject");
-
-						$body = sprintf(
-							$main_smarty->get_config_vars("PLIGG_PassEmail_PassBody"),
-							$main_smarty->get_config_vars("PLIGG_Visual_Name"),
-							$my_base_url . $my_pligg_base . '/login.php',
-							$user->user_login,
-							$password
-							);
-						
-						$headers = 'From: ' . $main_smarty->get_config_vars("PLIGG_PassEmail_From") . "\r\n";
-						$headers .= "Content-type: text/html; charset=utf-8\r\n";
-						
-						if (mail($to, $subject, $body, $headers))
-						{
-						  $saltedPass = generateHash($password);
-						  $db->query('UPDATE `' . table_users . "` SET `user_pass` = '$saltedPass' WHERE `user_login` = '".$user->user_login."'");
-						  $db->query('UPDATE `' . table_users . '` SET `last_reset_request` = FROM_UNIXTIME('.time().') WHERE `user_login` = "'.$user->user_login.'"');
-						
-						  $current_user->Authenticate($user->user_login, $password);
-						  $return =  my_pligg_base.'/';
-						  if(strpos($_SERVER['SERVER_SOFTWARE'], "IIS") && strpos(php_sapi_name(), "cgi") >= 0){
-								echo '<SCRIPT LANGUAGE="JavaScript">window.location="' . $return . '";</script>';
-								echo $main_smarty->get_config_vars('PLIGG_Visual_IIS_Logged_In') . '<a href = "'.$return.'">' . $main_smarty->get_config_vars('PLIGG_Visual_IIS_Continue') . '</a>';
-						   } else {
-								header('Location: '.$return);
-						    }
-						  
-						 } else{
-	
-							$errorMsg = $main_smarty->get_config_vars('PLIGG_Visual_Login_Delivery_Failed');
-	
-						}
+				$saltedlogin = generateHash($username);
 				
+				$to = $user->user_email;
+				$subject = $main_smarty->get_config_vars("PLIGG_Visual_Name").' '.$main_smarty->get_config_vars("PLIGG_PassEmail_Subject");
+
+				$body = sprintf(
+					$main_smarty->get_config_vars("PLIGG_PassEmail_PassBody"),
+					$main_smarty->get_config_vars("PLIGG_Visual_Name"),
+					$my_base_url . $my_pligg_base . '/login.php',
+					$user->user_login,
+					$password
+				);
+				
+				$headers = 'From: ' . $main_smarty->get_config_vars("PLIGG_PassEmail_From") . "\r\n";
+				$headers .= "Content-type: text/html; charset=utf-8\r\n";
+				
+				if (mail($to, $subject, $body, $headers))
+				{
+					$saltedPass = generateHash($password);
+					$db->query('UPDATE `' . table_users . "` SET `user_pass` = '$saltedPass' WHERE `user_login` = '".$user->user_login."'");
+					$db->query('UPDATE `' . table_users . '` SET `last_reset_request` = FROM_UNIXTIME('.time().') WHERE `user_login` = "'.$user->user_login.'"');
+				
+					$current_user->Authenticate($user->user_login, $password);
+					$return =  my_pligg_base.'/';
+					if(strpos($_SERVER['SERVER_SOFTWARE'], "IIS") && strpos(php_sapi_name(), "cgi") >= 0){
+						echo '<SCRIPT LANGUAGE="JavaScript">window.location="' . $return . '";</script>';
+						echo $main_smarty->get_config_vars('PLIGG_Visual_IIS_Logged_In') . '<a href = "'.$return.'">' . $main_smarty->get_config_vars('PLIGG_Visual_IIS_Continue') . '</a>';
+					} else {
+						header('Location: '.$return);
+					}
+					
+				} else {
+				
+					$errorMsg = $main_smarty->get_config_vars('PLIGG_Visual_Login_Delivery_Failed');
+					
+				}
+				
+			}
 			
-	
-	
+			$main_smarty->assign('form_password_error', $form_password_error);
 			
 		}
-		 $main_smarty->assign('form_password_error', $form_password_error);
-	  }
-	
+	  
 	}
 
-}else{
+} else {
+
 	$errorMsg = $main_smarty->get_config_vars('PLIGG_Password_Invalid_URL');
+	
 }
-  
-    
+
 // pagename
 define('pagename', 'recover'); 
 $main_smarty->assign('pagename', pagename);
