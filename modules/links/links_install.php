929f2249ee9aceb4d6cb651aa52f1b9848a13477
Eric Heikkinen <eric@pligg.com>
2013-07-16 16:06:47 -0700

    - Updating links module for release.

--- modules/links/links_install.php
+++ modules/links/links_install.php
@@ -1,7 +1,7 @@
 <?php
 	$module_info['name'] = 'Links';
-	$module_info['desc'] = 'Replaces http:// URLs in comments and stories with a clickable version.';
-	$module_info['version'] = 0.1;
+	$module_info['desc'] = 'Replaces "http://" URLs in comments and stories with a clickable URL.';
+	$module_info['version'] = 1.0;
 	$module_info['settings_url'] = '../module.php?module=links';
 	$module_info['homepage_url'] = 'http://pligg.com/downloads/module/links/';
 	$module_info['update_url'] = 'http://pligg.com/downloads/module/links/version/';
