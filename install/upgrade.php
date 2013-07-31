1950126f05702818426d72e390a3ff186282a7c7
Eric Heikkinen <eric@pligg.com>
2013-07-31 12:38:00 -0700

    - Fixed the local language version update files. I left many of the upgrade files in English until translators contribute updates.

--- install/upgrade.php
+++ install/upgrade.php
@@ -31,6 +31,18 @@ if($language == 'arabic'){
 	include_once('./languages/lang_english.php');
 }
 
+echo '<style type="text/css">
+h2 {
+margin:0 0 5px 0;
+line-height:30px;
+}
+.well {
+background:#ffffff;
+}
+
+</style>';
+echo '<div class="well">';
+
 if($language == '' && $_POST['submit'] == ''){
 	$data = file_get_contents('./languages/language_list_upgrade.html');
 
@@ -48,17 +60,13 @@ if($language == '' && $_POST['submit'] == ''){
 	die();
 
 } else {
-
 	$step = 1;
-
 }
-
 
 $include='header.php'; if (file_exists($include)) { include_once($include); }
 $include='functions.php'; if (file_exists($include)) { include_once($include); }
 
-
-echo '<h3>' . $lang['UpgradeTop'] . '</h3>';
+echo '<h2>' . $lang['Upgrade'] . '</h2>';
 
 $file='../config.php';
 if (!file_exists($file)) { $errors[]="$file " . $lang['NotFound'] ; }
@@ -104,108 +112,84 @@ if (!$errors) {
 	if ($old_version < '101'){
 		// User is using v1.0.0 or below
 		$oldLanguage100 = "// End 1.0.0 Language File";
-		$newLanguage101='./languages/updates/lang_' . $language . '_update_101.conf';
-		$r = new CD_HTTPRequest($newLanguage101);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_101.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage100 , $content , $languageContent);
 	}
 
 	if ($old_version < '102'){
 		$oldLanguage101 = "// End 1.0.1 Language File";
-		$newLanguage102='./languages/updates/lang_' . $language . '_update_102.conf';
-		$r = new CD_HTTPRequest($newLanguage102);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_102.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage101 , $content , $languageContent);
 	}
 	
 	if ($old_version < '103'){
 		$oldLanguage102 = "// End 1.0.2 Language File";
-		$newLanguage103='./languages/updates/lang_' . $language . '_update_103.conf';
-		$r = new CD_HTTPRequest($newLanguage103);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_103.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage102 , $content , $languageContent);
 	}
 	
 	if ($old_version < '104'){
 		$oldLanguage103 = "// End 1.0.3 Language File";
-		$newLanguage104='./languages/updates/lang_' . $language . '_update_104.conf';
-		$r = new CD_HTTPRequest($newLanguage104);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_104.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage103 , $content , $languageContent);
 	}
 	
 	if ($old_version < '105'){
 		$oldLanguage104 = "// End 1.0.4 Language File";
-		$newLanguage110='./languages/updates/lang_' . $language . '_update_110.conf';
-		$r = new CD_HTTPRequest($newLanguage110);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_105.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage104 , $content , $languageContent);
 	}
 	
 	if ($old_version < '111'){
 		$oldLanguage110 = "// End 1.1.0 Language File";
-		$newLanguage111='./languages/updates/lang_' . $language . '_update_111.conf';
-		$r = new CD_HTTPRequest($newLanguage111);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_111.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage110 , $content , $languageContent);
 	}
 
 	if ($old_version < '112'){
 		$oldLanguage111 = "// End 1.1.1 Language File";
-		$newLanguage112='./languages/updates/lang_' . $language . '_update_112.conf';
-		$r = new CD_HTTPRequest($newLanguage112);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_112.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage111 , $content , $languageContent);
 	}
 	
 	if ($old_version < '113'){
 		$oldLanguage112 = "// End 1.1.2 Language File";
-		$newLanguage113='./languages/updates/lang_' . $language . '_update_113.conf';
-		$r = new CD_HTTPRequest($newLanguage113);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_113.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage112 , $content , $languageContent);
 	}
 
 	if ($old_version < '114'){
 		$oldLanguage113 = "// End 1.1.3 Language File";
-		$newLanguage114='./languages/updates/lang_' . $language . '_update_114.conf';
-		$r = new CD_HTTPRequest($newLanguage114);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_114.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage113 , $content , $languageContent);
 	}
 	
 	if ($old_version < '115'){
 		$oldLanguage114 = "// End 1.1.4 Language File";
-		$newLanguage115='./languages/updates/lang_' . $language . '_update_115.conf';
-		$r = new CD_HTTPRequest($newLanguage115);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_115.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage114 , $content , $languageContent);
 	}
 	
-	if ($old_version < '116'){
+	if ($old_version < '120'){
 		$oldLanguage115 = "// End 1.1.5 Language File";
-		$newLanguage120='./languages/updates/lang_' . $language . '_update_120.conf';
-		$r = new CD_HTTPRequest($newLanguage120);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_120.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage115 , $content , $languageContent);
 	}
 	
 	if ($old_version < '121'){
 		$oldLanguage120 = "// End 1.2.0 Language File";
-		$newLanguage121='./languages/updates/lang_' . $language . '_update_121.conf';
-		$r = new CD_HTTPRequest($newLanguage121);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_121.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage120 , $content , $languageContent);
 	}
@@ -217,11 +201,9 @@ if (!$errors) {
 		$languageContent = str_replace($oldLanguage121 , $newLanguage122 , $languageContent);
 	}
 	
-	if ($old_version < '123'){
+	if ($old_version < '200'){
 		$oldLanguage122 = "// End 1.2.2 Language File";
-		$newLanguage200='./languages/updates/lang_' . $language . '_update_200.conf';
-		$r = new CD_HTTPRequest($newLanguage200);
-		$content = $r->DownloadToString();
+		$content = file_get_contents('./languages/updates/lang_' . $language . '_update_200.conf');
 		if($content)
 			$languageContent = str_replace($oldLanguage122 , $content , $languageContent);
 	}
@@ -323,6 +305,8 @@ else {
 	echo DisplayErrors($errors);
 	echo '<p>' . $lang['PleaseFix'] . '</p>';
 }
+
+echo '</div>'; // .well	
 
 $include='footer.php'; if (file_exists($include)) { include_once($include); }
 ?>
