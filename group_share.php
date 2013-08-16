c8ba342e1fc4a090f366ca9fc34bbb17d332329b
Eric Heikkinen <eric@pligg.com>
2013-08-16 13:17:07 -0700

    - Some indents

--- group_share.php
+++ group_share.php
@@ -22,18 +22,18 @@ if (isset($_GET['link_id']) && isset($_GET['group_id']))
     $privacy = $db->get_var("SELECT group_privacy FROM " . table_groups . " WHERE group_id = '$group_id';");
     if (($privacy!='private' || isMemberActive($group_id)=='active'))
     {
-	global $db, $current_user;
-	$current_userid = $current_user->user_id;
+		global $db, $current_user;
+		$current_userid = $current_user->user_id;
 
-	$sql = "INSERT IGNORE INTO ". table_group_shared ." ( `share_link_id` , `share_group_id`, `share_user_id` ) VALUES ('".$link_id."', '".$group_id."','".$current_userid."' ) ";
-	//echo $sql;
-	$results = $db->query($sql);
-	$redirect = '';
-	$redirect = getmyurl("group_story", $group_id);
-	header("Location: $redirect");
+		$sql = "INSERT IGNORE INTO ". table_group_shared ." ( `share_link_id` , `share_group_id`, `share_user_id` ) VALUES ('".$link_id."', '".$group_id."','".$current_userid."' ) ";
+		//echo $sql;
+		$results = $db->query($sql);
+		$redirect = '';
+		$redirect = getmyurl("group_story", $group_id);
+		header("Location: $redirect");
     } else {
-	$redirect = getmyurl("groups");
-	header("Location: $redirect");
+		$redirect = getmyurl("groups");
+		header("Location: $redirect");
     }
 }
 ?>
