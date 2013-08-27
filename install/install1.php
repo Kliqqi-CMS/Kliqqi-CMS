072b44e51b31477d310f6c8b38d7501fd6c647f0
Eric Heikkinen <eric@pligg.com>
2013-08-26 19:12:33 -0700

    - Changed the install welcome text block size

--- install/install1.php
+++ install/install1.php
@@ -1,12 +1,12 @@
 <div class="instructions">
-	<div class="jumbotron" style="padding:16px 25px;">
+	<div class="jumbotron" style="padding:6px 25px 16px 25px;">
 		<h2><?php echo $lang['WelcomeToInstaller']; ?></h2>
-		<p style="font-size:1.0em;">
+		<p style="font-size:0.8em;">
 			<?php echo $lang['WelcomeToThe']; ?>
 		</p>
 	</div>
 	<p><?php echo $lang['Bugs']; ?></p>
-	<br />
+	<hr />
 	<h3><?php echo $lang['Installation']; ?></h3>
 	<p><?php echo $lang['OnceFamiliar']; ?></p>
 	<form id="form2" name="form2" method="post" action="install.php">
