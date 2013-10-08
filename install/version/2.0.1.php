229492196711f7c784513d3ad19fecbd50669ab6
Eric Heikkinen <eric@pligg.com>
2013-10-08 14:51:39 -0700

    - 2.0.1 Upgrade will remove the option to customize the avatar upload directory. Re: issue #98 (https://github.com/Pligg/pligg-cms/issues/98)

--- install/version/2.0.1.php
+++ install/version/2.0.1.php
@@ -13,6 +13,12 @@ if ($pligg_version = '2.0.0') {
 
 	echo '<li>Performing one-time Pligg 2.0.1 Upgrade<ul>';
 	
+	// Remove the Spell Checker from Admin Config
+	$sql = "DELETE FROM " . table_config . " 
+			WHERE var_name='User_Upload_Avatar_Folder'";
+	$db->query($sql);
+	echo '<li>Removed Custom Avatar Directory Option</li>';
+	
 	// Update version number
 	$sql = "UPDATE `" . table_misc_data . "` SET `data` = '2.0.1' WHERE `name` = 'pligg_version';";
 	$db->query($sql);
