b1a8c1558e3e698388ce6cf2d2bf528226ddeba3
yankidank <eric@pligg.com>
2014-08-12 13:20:15 -0700

    - Sidebar Saved module bug fix: users were seeing any new favorites being added, not just their own. Thanks @redwinefireplace https://github.com/Pligg/pligg-cms/commit/17fb083ce61356dd506dec8ad359eafe2d2dc759#commitcomment-7362256

--- modules/sidebar_saved/update_rightpanel.php
+++ modules/sidebar_saved/update_rightpanel.php
@@ -10,8 +10,9 @@
 	include_once(mnminclude.'smartyvariables.php');
 
 	// -------------------------------------------------------------------------------------
-	
-	$res = "select link_id,link_title,saved_id,saved_user_id,saved_link_id from ".table_links.",".table_saved_links." WHERE saved_link_id = link_id ORDER BY saved_id DESC limit 5";
+	global $the_template, $main_smarty, $db;
+	$user_id = $main_smarty->get_template_vars('user_id');
+	$res = "select link_id,link_title,saved_id,saved_user_id,saved_link_id from ".table_links.",".table_saved_links." WHERE saved_user_id =$user_id and link_id = saved_link_id ORDER BY saved_id DESC limit 5";
 	$list_savedlinks = $db->get_results($res);
 	$html = "";
 	
