b80394e312e884e8801329b09f93005b590f0354
yankidank <eric@pligg.com>
2014-08-15 19:34:52 -0700

    - Simple Messaging module updated version number to 2.4. Removed a lot of older icons no longer in use.

--- modules/simple_messaging/simple_messaging_install.php
+++ modules/simple_messaging/simple_messaging_install.php
@@ -1,7 +1,7 @@
 ﻿<?php
 	$module_info['name'] = 'Simple Private Messaging';
 	$module_info['desc'] = 'Let users send private messages to each other.';
-	$module_info['version'] = 2.3;
+	$module_info['version'] = 2.4;
 	$module_info['homepage_url'] = 'http://pligg.com/downloads/module/simple-messaging/';
 	$module_info['update_url'] = 'http://pligg.com/downloads/module/simple-messaging/version/';
 
